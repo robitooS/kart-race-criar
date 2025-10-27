1a8bc572eb3d291e708dbbf2ed1096e78686d327
Higor Robin <proghigorrobin@gmail.com>
2025-10-27 13:22:12 -0300

    refactor: alterado nome de variáveis para melhor legibilidade e comentários mais objetivos na lógica do cod e nome do piloto

--- scripts/resultados_corrida_kart.py
+++ scripts/resultados_corrida_kart.py
@@ -14,17 +14,25 @@ df_results_columns = [
 ]
 df_results_race_kart = pd.DataFrame(columns=df_results_columns)
 
-# === 1. Lógica para código do piloto e seu respectivo nome ===
-piloto_col = list(df_race_kart_logs["Piloto"])
-cod_piloto = {}
+# === 1. Extrair código e nome do piloto ===
+cod_pilotos = {}
+for piloto_bruto in df_race_kart_logs["Piloto"]:
+    partes = piloto_bruto.split("-")
+    cod = partes[0].strip()
+    nome = partes[-1].strip()
+    cod_pilotos[cod] = nome
 
-for p in piloto_col:
-    piloto = p.split("-")
-    cod_piloto[piloto[0].strip()] = piloto[-1].strip()
+# Criar coluna auxiliar no DF original com apenas o código do piloto
+df_race_kart_logs["Cod_Piloto"] = df_race_kart_logs["Piloto"].apply(
+    lambda p: p.split("-")[0].strip()
+)
 
-df_results_race_kart["Código do piloto"] = cod_piloto.keys()
-df_results_race_kart["Nome do piloto"] = cod_piloto.values()
-print(df_results_race_kart)
+# Inserir no df resultante o código e nome do piloto
+df_results_race_kart["Código do piloto"] = list(cod_pilotos.keys())
+df_results_race_kart["Nome do piloto"] = list(cod_pilotos.values())
+
+print(f"\n************** DF RESULTANTE APÓS NOME E CÓDIGO DO PILOTO **************\n{df_results_race_kart}\n")
+print(f"\n************** DF INICIAL **************\n{df_race_kart_logs}\n")
 
 # === 2. Contar voltas por piloto ===
 qtd_voltas_series = df_race_kart_logs["Piloto"].value_counts()
