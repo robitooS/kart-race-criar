0c323a73f3669e75a85fee330c0e4711f3ff0ebb
Higor Robin <proghigorrobin@gmail.com>
2025-10-27 13:21:06 -0300

    style: melhorando legibilidade e comentários

--- scripts/resultados_corrida_kart.py
+++ scripts/resultados_corrida_kart.py
@@ -1,16 +1,18 @@
 import pandas as pd
 
 # Carregar o arquivo csv contendo os dados da corrida de kart
-df_race_kart_logs = pd.read_csv("data\log_kart.csv", delimiter=";") # Utilizar ; como delimitador
+df_race_kart_logs = pd.read_csv("data/log_kart.csv", delimiter=";")
+print(f"\n************** KART LOG INICIAL **************\n{df_race_kart_logs}\n")
 
+# Instanciando df resultante com as respectivas colunas
 df_results_columns = [
     "Posição de chegada",
     "Código do piloto",
-    "Nome do piloto", 
+    "Nome do piloto",
     "Quantidade de voltas completadas",
     "Tempo total de prova"
-] # Colunas do df resultante
-df_results_race_kart = pd.DataFrame(columns=df_results_columns) # Instanciando df resultante
+]
+df_results_race_kart = pd.DataFrame(columns=df_results_columns)
 
 # === 1. Lógica para código do piloto e seu respectivo nome ===
 piloto_col = list(df_race_kart_logs["Piloto"])
